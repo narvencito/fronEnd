0a83bdab380269980e22594da8ee323597a56673
narvencito <narvencito@gmail.com>
2018-07-02 00:45:58 -0500

    creacion del routing y el paquete usuario

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { AppComponent } from '../app.component';
+
+const routes: Routes=[
+  {path:'', redirectTo:'/appComponent', pathMatch:'full'},
+  {path:'appComponent',component:AppComponent}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ],
+
+  declarations: []
+})
+export class AppRoutingModule { }
