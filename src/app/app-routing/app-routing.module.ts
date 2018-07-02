72a6587094c2842a763d364576c23fb0f91f2614
narvencito <narvencito@gmail.com>
2018-07-02 01:55:39 -0500

    modulo usuario creado

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import { AppComponent } from '../app.component';
 
-const routes: Routes=[
-  {path:'', redirectTo:'/appComponent', pathMatch:'full'},
-  {path:'appComponent',component:AppComponent}
+import { AppComponent } from '../app.component';
+import { UserComponent } from '../user/user.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/userComponent', pathMatch: 'full'},
+  {path: 'appComponent', component: AppComponent},
+  {path: 'userComponent', component: UserComponent}
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
   ],
-  exports:[
+  exports: [
     RouterModule
   ],
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,7 +6,7 @@ import { UserService } from './user.service';
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css'],
-  providers:[UserService]
+  providers: [UserService]
 })
 export class UserComponent implements OnInit {
 
